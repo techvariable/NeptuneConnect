e01a6ce2c22cdad4b53111ed37db2c455deceeb8
Abhishek Chatterjee <abhishek@techvariable.com>
2023-07-04 13:26:04 +0530

    made swagger configurable

--- src/fastify/fastifyPlugins/swaggerUi.ts
+++ src/fastify/fastifyPlugins/swaggerUi.ts
@@ -2,7 +2,10 @@ import fp from 'fastify-plugin'
 import swaggerUI, { type FastifySwaggerUiOptions } from '@fastify/swagger-ui'
 
 export default fp<FastifySwaggerUiOptions>(async (fastify) => {
-  await fastify.register(swaggerUI, {
-    routePrefix: '/docs'
-  })
+  console.log(process.env.SWAGGER_ENABLED === '1', process.env.SWAGGER_ROUTE)
+  if (process.env.SWAGGER_ENABLED === '1') {
+    await fastify.register(swaggerUI, {
+      routePrefix: process.env.SWAGGER_ROUTE
+    })
+  }
 })
